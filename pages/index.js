bf73f4cfc2e0859e9d773be11d7bdf7ea0001cc0
Danny Mousa <dannymou2@gmail.com>
2022-08-08 11:05:50 +0300

    fix env var problem

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import RecipeCard from "../components/RecipeCard";
 export const getStaticProps = async () => {
   const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
   });
 
   const res = await client.getEntries({ content_type: "recipe" });
